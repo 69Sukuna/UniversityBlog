cbe3aa36e363855d815964153544d30feccf104a
69Sukuna <108551296+69Sukuna@users.noreply.github.com>
2026-05-29 21:02:12 -0400

    fix: node 22 and site url
    fix: node 22 and site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  site: 'https://university-blog-o80g09i4u-gatos-projects-b46d88be.vercel.app',
   integrations: [db(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
